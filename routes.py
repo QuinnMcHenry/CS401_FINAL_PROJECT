33828f7eb84e8f036923b8c11dd26a96d81b0b99
LoganLasell <llasell48@gmail.com>
2025-04-04 11:39:48 -1000

    updates api and html

--- routes.py
+++ routes.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/map")
+def get_map():
     return render_template("map.html")
 
 @app.route('/arrivals', methods=['GET'])
